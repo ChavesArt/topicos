1dfde8d350befbe0950297727c55057c14640495
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-08-02 09:41:09 -0300

    fazendo o materialize

---
+++ destinos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include "links.php"; ?>
+
+</head>
+<body>
+    <?php include "menu.php"; ?>
+    <h1>Destinos</h1>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
+    
+    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,16 @@
+<div class ="navbar-fixed">
+
+        <nav class="grey lighten-1">
+            <div class="nav-wrapper">
+                <a href="index.php" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a class = "active" href="index.php">Home</a></li>
+                    <li><a href="quem.php">Quem somos</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="destinos.php">Destinos</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+    
+    </div>
