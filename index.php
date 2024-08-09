b4687bb625470ffb8aaceb75cc953548dc55f69d
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-08-09 09:41:25 -0300

    materialize

--- index.php
+++ index.php
@@ -3,25 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TOPICOS</title>
     <?php include "links.php"; ?>
 </head>
 <body>
         <?php include "menu.php"; ?>
+        <div class="container">
         <h1>Início</h1>
+        <div class="row">
+      <div class="col s1 red">1</div>
+      <div class="col s1 blue">2</div>
+      <div class="col s1 red">3</div>
+      <div class="col s1 blue">4</div>
+      <div class="col s1 red">5</div>
+      <div class="col s1 blue">6</div>
+      <div class="col s1 red">7</div>
+      <div class="col s1 blue">8</div>
+      <div class="col s1 red">9</div>
+      <div class="col s1 blue">10</div>
+      <div class="col s1 red">11</div>
+      <div class="col s1 blue">12</div>
+      <!-- /row -->
+    </div>
 
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur vel impedit consequuntur modi excepturi ducimus, ullam velit nostrum, ipsa minus quae explicabo iste perferendis, eius veniam nisi? Assumenda, incidunt consectetur.
-    
+    <div class="row">
+      <div class="col s12 red">1</div>
+      <div class="col s6 blue">2</div>
+      <div class="col s6 red">3</div>
+      <div class="col s12 blue">4</div>
+
+      <!-- /row -->
+    </div>
+    <div class="row">
+      <div class="col s3 red offset-s4">1</div>
+
+
+      <!-- /row -->
+    </div>
+    <!-- container -->
+    </div>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -2,7 +2,10 @@
 
         <nav class="grey lighten-1">
             <div class="nav-wrapper">
-                <a href="index.php" class="brand-logo">Logo</a>
+                <a href="index.php" class="brand-logo">
+                        <img src="furia.jpg" alt="logo" width="55px" height="60px">
+                </a>
+
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><a class = "active" href="index.php">Home</a></li>
                     <li><a href="quem.php">Quem somos</a></li>
