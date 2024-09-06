eaea21cd5b2857d788c6591eab47526f40d9cc29
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-09-06 09:25:11 -0300

    fazendo um modal

--- ursula/turismo/clientes04.php
+++ ursula/turismo/clientes04.php
@@ -1,69 +1,104 @@
 <?php
 include "conect.php";
 
-$sql = "SELECT * FROM cliente ORDER BY nomeCliente DESC";
+$sql = "SELECT * FROM usuario ORDER BY nomeCliente DESC";
 
-$resultado = mysqli_query($conexao,$sql);
+$resultado = mysqli_query($conexao, $sql);
 ?>
 
 
 <!DOCTYPE html>
-  <html>
-  <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<html>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
 
-    <body>
-   
-    <?php 
-    include_once "header.php";
-    include "conect.php";
-  
-    ?>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 
-   <main class="container">  
-  <h1> Clientes </h1>
+<body>
 
-  <table class = "striped">
-        <thead>
-          <tr>
-              <th>ID</th>
-              <th>CPF</th>
-              <th>Nome</th>
-              <th>Data de nascimento</th>
-              <th>Operação</th>
-          </tr>
-        </thead>
+  <?php
+  include_once "header.php";
+  include "conect.php";
 
-        <tbody>
+  ?>
+
+  <main class="container">
+    <h1> Clientes </h1>
+
+    <table class="striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>CPF</th>
+          <th>Nome</th>
+          <th>Data de nascimento</th>
+          <th>Operação</th>
+        </tr>
+      </thead>
+
+      <tbody>
         <?php
 
-while ($cliente = mysqli_fetch_assoc($resultado)) {
-  $date = date_create($cliente['dataNasc']);
-    echo '<tr>';
+        while ($cliente = mysqli_fetch_assoc($resultado)) {
+          $date = date_create($cliente['dataNasc']);
+        ?>
+          <tr>
 
-    echo '<td>' . $cliente['id_cliente'] . '</td>';
-    echo "<td>" . $cliente['cpf'] . "</td>";
-    echo '<td>' . $cliente['nomeCliente'] . '</td>';
-    echo "<td>" . date_format($date, "d/m/Y");
-    // echo "<td>" . $cliente['dataNasc'] . "</td>";
-    echo"<td>" . "<a class='btn-floating btn-medium waves-effect waves-light red'><i class='material-icons'>delete</i></a>"."</td>"; 
+            <td> <?php echo $cliente['id_cliente']; ?> </td>
+            <td> <?php echo $cliente['cpf']; ?> </td>
+            <td> <?php echo $cliente['nomeCliente'] ?> </td>
+            <td> <?php echo date_format($date, "d/m/Y"); ?>
+            <td> <a href='#modal<?php echo $cliente['id_cliente']; ?>' class='btn-floating btn-medium waves-effect waves-light red modal-trigger'><i class='material-icons'>delete</i></a> </td>
 
-    echo '</tr>';
-}
+            <!-- // modal estrutura -->
+            <div id='modal<?php echo $cliente['id_cliente']; ?>' class='modal'>
+              <div class='modal-content'>
+                <h4>Atenção!</h4>
+                <p>Você confirma a exclusão do cliente?<?php echo  $cliente['nomeCliente']; ?></p>
+              </div>
+              <div class='modal-footer'>
+                <form action="excluir.php" method="post">
+                  <button type="submit" name="btn-deletar" class="modal-action modal-close waves-red btn green darken-1">
+                    Excluir </button>
 
-?>
-        </tbody>
-      </table>
+                  <button type="button" name="btn-cancelar" class="modal-action modal-close  btn waves-light red">
+                    Cancelar </button>
+                </form>
+              </div>
+            </div>
 
-</main>
+          </tr>
 
-<script src="js/materialize.min.js"></script>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+
+  </main>
+
+  <script src="js/materialize.min.js"></script>
+
+  <script>
+    // M.AutoInit();
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems,{
+        opacity: 0.7, //opacidade do background(0.0.1.0)
+        inDuration: 1000, //duração da animação de abertura em milisegundos
+        outDuration: 1200, //duração da animação de fechamento em milisegundos
+        dismissible: false, //Permite o fechar ao clicar fora do modal
+        startingTop:'10%', //Posição inicial do modal em relação ao topo
+        endingTop:'15%', //Posição final do modal em relação ao topo
+      });
+    });
+  </script>
 </body>
+
 </html>
