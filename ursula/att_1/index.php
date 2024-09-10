1937c22e6fb0ab4f4d98876031493b4f1974e377
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-09-10 08:26:32 -0300

    att_1 de topicos com o Luiz em andamento

---
+++ ursula/att_1/index.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+
+<header>
+
+<nav>
+    <div style="height: 50px;" class="nav-wrapper container">
+      <a href="#" class="brand-logo"><img src="img/messi_logo.jfif" alt="messi anão" width="65px" height="65px"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Clientes</a></li>
+        <li><a href="#">Sobre</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</header>
+
+    <main>
+    <div class="row">
+
+    <div  class="col s12 m4">
+      <div class="card">
+      <span class="card-title">91 gol em uma temporada</span>
+        <div class="card-image">
+          <img src="img/91_messi.jpg">    
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="card ">
+      <span class="card-title">Conquista da copa do mundo</span>
+        <div class="card-image">
+          <img src="img/messi_copa.jpg">
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Mais detalhes</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="card ">
+      <span class="card-title">Ancarã messi</span>
+        <div class="card-image">
+          <img src="img/ancara_messi.jpg">
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Mais detalhes</a>
+        </div>
+      </div>
+    </div>
+
+    <div  class="col s12 m4">
+      <div class="card ">
+      <span class="card-title">7 vezes melhor do mundo</span>
+        <div class="card-image">
+          <img src="img/7_vezes.jpeg">    
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Mais detalhes</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="card ">
+      <span class="card-title">MSN!</span>
+        <div class="card-image">
+          <img src="img/msn.jpg">
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Mais detalhes</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="card">
+      <span class="card-title">Ancarã messi</span>
+        <div class="card-image">
+          <img src="img/messi_ronaldinho.jpg">
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">Mais detalhes</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+    </main>
+
+
+    <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+
+              <div class="col l4 s12">
+                <h5 class="white-text">Endereço</h5>
+                <p class="grey-text text-lighten-4">Inter Miami,Bur Khalifa.</p>
+              </div>
+
+              <div class="col l4 s12">
+                <h5 class="white-text">Paternidade</h5>
+                <p>Cristiano Ronaldo.</p>
+              </div>
+
+              <div class="col l4 s12">
+                <h5 class="white-text">Redes sociais</h5>
+                <p class="grey-text text-lighten-4"> <i class="material-icons">facebook</i></p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container" style="text-align:center;">
+            © 2014 Copyright Text
+            </div>
+          </div>
+        </footer>
+
+</body>
+</html>
